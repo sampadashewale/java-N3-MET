05759d2987d48098cd4e565331d05db252790819
sampada shewlae <sampadashewale1@gmail.com>
2023-03-03 12:21:53 +0530

    Ass3 commit

---
+++ Assignment3/src/org/tnsif/designpattern/Singleton.java
@@ -0,0 +1,13 @@
+package org.tnsif.designpattern;
+
+public class Singleton {
+	
+	private static final Singleton INSTANCE = new Singleton();
+	private Singleton() {
+		
+	}
+	public static Singleton getInstance() {
+		return INSTANCE;
+	}
+
+}
